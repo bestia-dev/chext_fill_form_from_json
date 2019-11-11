407ad25da0bd02d9b3c709d6ac7787529b617043
Luciano Bestia <luciano.bestia@gmail.com>
2019-11-11 11:50:44 +0530

    publish

--- inject.js
+++ inject.js
@@ -69,7 +69,7 @@ var injected = injected || (function () {
                     var opts = el.options;
                     for (var opt, j = 0; opt = opts[j]; j++) {
                         if (opt.value == val) {
-                            alert(opt.value);
+                            //alert(opt.value);
                             el.selectedIndex = j;
                             break;
                         }
